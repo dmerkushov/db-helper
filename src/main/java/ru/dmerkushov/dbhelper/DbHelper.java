13452c3fe55cecc7161a481075e2bf8fa40aad50
dmerkushov <dmerkushov@localhost>
2013-12-19 23:42:01 +0400

    1. Added license notice 2. Added recordExists()

--- src/main/java/ru/dmerkushov/dbhelper/DbHelper.java
+++ src/main/java/ru/dmerkushov/dbhelper/DbHelper.java
@@ -1,7 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 dmerkushov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package ru.dmerkushov.dbhelper;
 
 import java.sql.*;
@@ -17,7 +29,7 @@ public class DbHelper {
 	Connection dbConnection = null;
 	String driverName = null;
 	String connectionUrl = null;
-	LoggerWrapper loggerWrapper = LoggerWrapper.getLoggerWrapper ("DbHelper");
+	static LoggerWrapper loggerWrapper = LoggerWrapper.getLoggerWrapper ("DbHelper");
 
 	/**
 	 *
@@ -364,6 +376,27 @@ public class DbHelper {
 		
 		return result;
 	}
+	
+	/**
+	 * Check if a record exists
+	 * @param sql
+	 * @param params
+	 * @return
+	 * @throws DbHelperException
+	 * @throws SQLException 
+	 */
+	public boolean recordExists (String sql, Object[] params) throws DbHelperException, SQLException {
+		loggerWrapper.entering (sql, params);
+		
+		boolean exists;
+		try (ResultSet existsRs = performDbQuery (sql, params)) {
+			exists = existsRs.next ();
+		}
+		
+		loggerWrapper.exiting (exists);
+		return exists;
+	}
+
 
 	/**
 	 * Perform an update to the database
